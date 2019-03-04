dc0f37210083dd34bdd5a3e3a7c750262ebe8bde
unknown <jonathanmohan.net@gmail.com>
2019-03-04 16:10:52 -0600

    added favorite things classes

---
+++ MyFavoriteStuff/NesGames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFavoriteStuff
+{
+    class NesGames
+    {
+        public string Name { get; }
+        public string Genre { get; }
+        public int Year { get; }
+
+        public NesGames(string name, string genre, int year)
+        {
+            Name = name;
+            Genre = genre;
+            Year = year;
+        }
+    }
+}
